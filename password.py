f909f6b722749099cfe68081b6a020590c011c7b
tuandao <ngoctuan.dao@my.jcu.edu.au>
2023-07-17 13:48:36 +0800

    updated

--- password.py
+++ password.py
@@ -1,1 +1,9 @@
-print("Password")
+MINIMUM_CHAR = 0
+
+password = input("Enter your password: ")
+
+while len(password) <= MINIMUM_CHAR:
+    print("Invalid Password")
+    password = input("Enter your password: ")
+print("*" * len(password))
+
